0bb5ce03a9a5333c40d08ec35421d35ae63d04f3
olivia92 <36474665+olivia92@users.noreply.github.com>
2018-05-05 19:00:53 +0200

    Add files via upload

---
+++ formexp.php
@@ -0,0 +1,95 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>Vous</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="carousel.css" rel="stylesheet">
+  </head>
+  <body>
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-navy">
+      <a class="navbar-brand" href="#"><img src="http://exetertri.co.uk/wp-content/uploads/2015/10/swim-logo.png" alt="" width="45" height="45"></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Recherche" aria-label="Search">
+          <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Recherche</button>
+        </form>
+      </ul>
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Accueil</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Réseaux</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Emplois</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Messagerie</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Notifications</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Vous</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Album</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Déconnexion</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+      <div class="jumbotron">
+        <div class="container">
+          <h1 class="display-3">Enrichissez votre profil</h1>
+        </div>
+      </div>
+      
+        <form class="needs-validation" method="post" novalidate>
+            <div class="form-group">
+            <div class="col-md-12 mb-3">
+              <label for="poste"> Poste </label>
+              <input name="poste" type="texte" class="form-control" id="poste" placeholder="">
+            </div>
+             <div class="col-md-12 mb-3">
+              <label for="mission"> Mission réalisée </label>
+              <input name="missoin" type="texte" class="form-control" id="mission" placeholder="">
+            </div>
+            <div class="col-md-12 mb-3">
+              <label for="entreprise"> Entreprise </label>
+              <input name="entreprise" type="texte" class="form-control" id="entreprise" placeholder="">
+            </div>
+            </div>
+            <button type="submit" name="submit" class="btn btn-primary center">Ajouter</button>
+        </form>
+
+      
+      
+      
+    <footer class="my-5 pt-md-5 border-top text-muted text-center text-small">
+      <div class="container">
+        <img class="mb-2" src="http://exetertri.co.uk/wp-content/uploads/2015/10/swim-logo.png" alt="" width="24" height="24">
+        <p class="mb-1">&copy; 2017-2018 Swim</p>
+      </div>
+    </footer>
+  </body>
+</html>
